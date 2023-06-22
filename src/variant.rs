766b07c1f436bd2c2e0530042dca0cc41603415f
Valerie <sleeplessval@gmail.com>
2023-06-22 11:25:16 -0400

    corrected 8-color flag colors

--- src/variant.rs
+++ src/variant.rs
@@ -6,13 +6,15 @@ use crate::{
 
 pub fn gilbert_baker() -> Colors {
 	let pink	= rgb(0xFF69B4);	//	sex
+	let red		= rgb(0xFF0000);	//	life
+	let orange	= rgb(0xFF8F00);	//	healing
+	let yellow	= rgb(0xFFFF00);	//	sunlight
+	let green	= rgb(0x008F00);	//	nature
 	let cyan	= rgb(0x00C0C0);	//	magic
+	let indigo	= rgb(0x3E0099);	//	serenity
+	let purple	= rgb(0x8F008F);	//	spirit
 
-	let mut output = flag::pride();
-	output.insert(0, pink);
-	output.insert(5, cyan);
-
-	output
+	vec![pink, red, orange, yellow, green, cyan, indigo, purple]
 }
 
 pub fn philadelphia() -> Colors {
